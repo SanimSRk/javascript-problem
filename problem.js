187d37fa958a418f7804a3b7cd5d994fec61cda8
SanimSRk <hasansanim562@gmail.com>
2024-02-08 00:21:42 +0600

    problem number 3 is solving is done

--- problem.js
+++ problem.js
@@ -1,5 +1,5 @@
 //frist problem solving now started
-
+/*
 function calculateMoney(ticketSale) {
   if (ticketSale < 0) {
     return '“Invalid Number”';
@@ -14,4 +14,41 @@ function calculateMoney(ticketSale) {
   const tottelTcketPrice = ticketRemining - loseMoney;
   return tottelTcketPrice;
 }
-console.log(calculateMoney(1));
+*/
+
+//second problem in javascript stat now
+
+/*
+function checkName(name) {
+  if (typeof name !== 'string') {
+    return 'invalid';
+  }
+  let word = ['a', 'y', 'i', 'e', 'o', 'u', 'w'];
+  const lastLatter = name[name.length - 1].toLowerCase();
+  for (const num of lastLatter)
+    if (word.includes(num)) {
+      return 'Good Name';
+    } else {
+      return 'Bad Name';
+    }
+}
+*/
+
+//thride problem solving state
+
+function deleteInvalids(array) {
+  if (!Array.isArray(array)) {
+    return 'Invalid Array';
+  }
+  let number = [];
+  for (const num of array) {
+    if (typeof num === 'number' && !isNaN(num)) {
+      number.push(num);
+    } else if (typeof num !== 'number') {
+      delete num;
+    }
+  }
+  return number;
+}
+
+console.log(deleteInvalids([1, 2, 3]));
